3747c6599874f883ee66a5f7aad6c1b1b70ff306
Tsvetoslav Vasilev <tvasilev@c4c2c3.42berlin.de>
2023-07-26 20:01:37 +0200

    comments added

--- test.spec.cy.js
+++ test.spec.cy.js
@@ -15,16 +15,16 @@
 //   }
 // }
 
-// function changeAfterFoobar(inputString, newValue) {
-//   const foobarIndex = inputString.indexOf("foobar=");
-//   if (foobarIndex !== -1) {
-//     const prefix = inputString.substring(0, foobarIndex + 7);
-//     const postfix = inputString.substring(foobarIndex + 7);
-//     return prefix + newValue;
-//   } else {
-//     return inputString;
-//   }
-// }
+function changeAfterFoobar(inputString, newValue) {
+  const foobarIndex = inputString.indexOf("foobar=");
+  if (foobarIndex !== -1) {
+    const prefix = inputString.substring(0, foobarIndex + 7);
+    const postfix = inputString.substring(foobarIndex + 7);
+    return prefix + newValue;
+  } else {
+    return inputString;
+  }
+}
 
 function generateRandomText(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -39,6 +39,9 @@ function generateRandomText(length) {
 }
 
 describe('Test link "Reflected XSS Example 1"', () => {
+  //example (https...?foobar=var1&foobar=var2&foobar=var3...&foobar=var'numberOfVars')
+  //number of vars is random and the var value is also randomly generated
+  //then this code makes sure that the variables are also present in the body of the website
   it('Open link with a bunch of variables', () => {
     let numberOfVars = Math.floor(1 + Math.random() * 9)
     let link = 'https://app-web-berlin42-50e7ace7d4d7.herokuapp.com/reflected_xss'
@@ -57,6 +60,10 @@ describe('Test link "Reflected XSS Example 1"', () => {
       cy.get('body').contains(decodedQueryString);
     });
   })
+  //from the main page it opens the link to the XSS 1
+  //then it injects an alert simply by changing the link's value
+  //then we expect to not have an alert??(or expect to have(we did't decide yet))
+  //? do we expect the website to have vulnerabilities or not (while testing)
   it('XSS attack', () => {
     cy.visit('https://app-web-berlin42-50e7ace7d4d7.herokuapp.com/')
     cy.get(':nth-child(1) > a').click()
--- testXSS2.cy.js
+++ testXSS2.cy.js
@@ -13,6 +13,7 @@ function changeAfterFoobar(inputString, newValue) {
 }
 
 describe('Test link "Reflected XSS Example 2"', () => {
+  //Inject an XSS attack by closing the div class with (">\")infront of the injection
   it('XSS attack', () => {
     cy.visit('https://app-web-berlin42-50e7ace7d4d7.herokuapp.com/')
     cy.get(':nth-child(1) > a').click()
