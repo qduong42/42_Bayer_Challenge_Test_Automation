1a2051b61905c2c19f54c79bdbccd93d156c98d3
Tsvetoslav Vasilev <tvasilev@c4c2c3.42berlin.de>
2023-08-03 17:16:53 +0200

    CSRF local open html

--- cypress/e2e/CSRF.cy.js
+++ cypress/e2e/CSRF.cy.js
@@ -11,7 +11,15 @@ describe('CSRF testing', () => {
     cy.get('[type="submit"]').click()
     cy.get('body').contains("999999999911120302104210491204021")
   })
-  it('CSRF attack', () => {
+  it('CSRF attack (local)', () => {
+    cy.visit('misc/CSRF_hosted_website.html')
+    cy.origin('https://app-web-berlin42-50e7ace7d4d7.herokuapp.com', () => {
+        cy.visit('');
+        cy.get(':nth-child(5) > a').click()
+        cy.get('body').should('not.contain', "Attacker account number")
+    })
+  })
+  it('CSRF attack (hosted)', () => {
     cy.visit('https://app-web-berlin42-50e7ace7d4d7.herokuapp.com/')
     cy.get(':nth-child(5) > a').click()
     cy.get('[name="account_number"]').type("123456")
