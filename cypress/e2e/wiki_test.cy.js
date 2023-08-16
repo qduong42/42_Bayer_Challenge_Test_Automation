83cc57a13a91301b6ee8413c27df8b30db961997
Cosmo <htam@student.42berlin.de>
2023-08-16 15:11:45 +0200

    meged wiki test case

--- cypress/e2e/wiki_test.cy.js
+++ cypress/e2e/wiki_test.cy.js
@@ -1,4 +1,19 @@
+/// <reference types="cypress" />
+
 describe('wiki_search', () => {
+
+  it('visit', () => {
+    cy.visit('https://en.wikipedia.org/wiki/Main_Page')
+  })
+
+  it('fuzz', () => {
+    cy.fuzz_attack('https://en.wikipedia.org/wiki/Main_Page', 50);
+  })
+  
+  it('link', () => {
+    cy.req_test('https://en.wikipedia.org/wiki/Main_Page')
+  })
+
   it.only('check result', () => {
     cy.fixture('wiki_list.json').then((data) => {
       const list = data.list
